484d5342c7f409b00e35b6c50cea1f077aaf065f
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2023-11-05 15:32:16 -0600

    fully implement shortcutter app; add a message box and clear button

--- shortcutter.py
+++ shortcutter.py
@@ -1,5 +1,5 @@
 import sys
-from tkinter import PhotoImage, StringVar, Tk, filedialog, ttk
+from tkinter import PhotoImage, StringVar, Tk, filedialog, messagebox, ttk
 
 from data.constants import DESKTOP_TEMPLATE
 from models.desktop_file_entry_form import FileContentsEntryForm
@@ -9,81 +9,100 @@ class ShortcutterApp:
     def __init__(self) -> None:
         self.root = Tk()
         self.root.geometry('400x300')
-        self.root.title = ('Shortcutter')
 
-        self.title_label = ttk.Label(
-            self.root, name='title', text="Welcome to Shortcutter")
-        self.title_label.pack()
+        # TEH TODO: plenty to do with regard to formatting andf making it look pretty..
+
+        # self.title_label = ttk.Label(
+        #     self.root, name='title', text='Welcome to Shortcutter')
+        # # self.title_label.pack(anchor='n')
+        # self.title_label.grid(column=0, row=0)
 
         self.selected_app = StringVar(value=None)
-        self.app_entry = ttk.Entry(
-            self.root, name='app_entry', textvariable=self.selected_app)
-        self.app_entry.pack()
+        self.app_label = ttk.Label(
+            self.root, name='app_label', textvariable=self.selected_app)
+        # self.app_label.pack(anchor='e')
+        self.app_label.grid(column=1, row=1)
         self.select_app_btn = ttk.Button(
-            self.root, name='select_app_btn', text="Select from File", command=self.select_application)
-        self.select_app_btn.pack()
+            self.root, name='select_app_btn', text='Select Application', command=self.select_application)
+        # self.select_app_btn.pack(anchor='w', padx=10, pady=10)
+        self.select_app_btn.grid(column=0, row=1, padx=10, pady=10)
 
         self.selected_icon = StringVar(value=None)
-        self.icon_entry = ttk.Entry(
-            self.root, name='icon_entry', textvariable=self.selected_icon)
-        self.icon_entry.pack()
+        self.icon_label = ttk.Label(
+            self.root, name='icon_label', textvariable=self.selected_icon)
+        # self.icon_label.pack(anchor='e')
+        self.icon_label.grid(column=1, row=2)
         self.select_icon_btn = ttk.Button(
-            self.root, name='select_icon_btn', text='Select from File', command=self.select_icon)
-        self.select_icon_btn.pack()
+            self.root, name='select_icon_btn', text='Select Icon', command=self.select_icon)
+        # self.select_icon_btn.pack(anchor='w', padx=10, pady=10)
+        self.select_icon_btn.grid(column=0, row=2, padx=10, pady=10)
 
         self.generate_btn = ttk.Button(
             self.root, text='Generate .desktop file', command=self.generate_desktop_file)
-        self.generate_btn.pack()
+        # self.generate_btn.pack()
+        self.generate_btn.grid(column=0, row=3, padx=10)
+
+        self.clear_btn = ttk.Button(
+            self.root, text='Clear selections', command=self.clear_selections)
+        # self.clear_btn.pack(anchor='s')
+        self.clear_btn.grid(column=0, row=4, padx=10, pady=3)
+
+    def show_message(self, message):
+        messagebox.showinfo(message=message)
 
     def select_application(self):
         file_types = (('All files', '*.*'), )
         selected_app_path = filedialog.askopenfilename(
-            initialdir='/opt/cursor', filetypes=file_types)
+            initialdir='~', filetypes=file_types)
 
         self.selected_app.set(selected_app_path)
 
     def select_icon(self):
         file_types = (('png files', '*.png'), )
         selected_icon_path = filedialog.askopenfilename(
-            initialdir='/home/tomhillenbrand', filetypes=file_types)
+            initialdir='~', filetypes=file_types)
 
         self.selected_icon.set(selected_icon_path)
-        # TEH TODO: display the icon image
-        # icon_image = PhotoImage(file=selected_icon_path)
-        # icon_label['image'] = icon_image
-        # icon_label.image = icon_image
-        # icon_label['text'] = selected_icon_path
-        # icon_label.text = selected_icon_path
-        # icon_label.pack()  # (column=1, row=3)
+        icon_image = PhotoImage(file=selected_icon_path)
+        self.icon_label['image'] = icon_image
+        self.icon_label.image = icon_image
 
     def generate_desktop_file(self):
 
-        dialog = FileContentsEntryForm(self.root)
+        if (self.selected_app.get() and self.selected_app.get() != '' and
+                self.selected_icon.get() and self.selected_icon != ''):
 
-        for widget in self.root.winfo_children():
-            if isinstance(widget, ttk.Entry):
-                breakpoint()
-                # if widget._name == '!label':
-                #     icon_path = widget['text']
-                # elif widget._name == '!label2':
-                #     exec_path = widget['text']
+            dialog = FileContentsEntryForm(self.root)
 
-        file_content = DESKTOP_TEMPLATE.format(
-            entry_type="Application",
-            app_name=dialog.app_name,
-            version=dialog.version,
-            comment=dialog.comment,
-            # path_to_executable="hello",
-            # executable=exec_path,
-            # icon_path=icon_path,
-            is_terminal="False",
-            categories=dialog.categories)
+            file_content = DESKTOP_TEMPLATE.format(
+                entry_type='Application',
+                app_name=dialog.app_name,
+                version=dialog.version,
+                comment=dialog.comment,
+                executable=self.selected_app.get(),
+                icon_path=self.selected_icon.get(),
+                is_terminal='False',
+                categories=dialog.categories)
 
-        file_name = f'/usr/share/applications/{dialog.app_name.lower()}.desktop'
-        # desktop_file = open(file_name, 'w')
-        # desktop_file.write(file_content)
-        print(file_name)
-        print(file_content)
+            file_name = f'/usr/share/applications/{dialog.app_name.lower()}.desktop'
+            desktop_file = open(file_name, 'w')
+            desktop_file.write(file_content)
+
+            self.show_message(
+                f'.desktop file created successfully: {file_name}')
+
+        elif (self.selected_app.get()):
+            self.show_message("Please select an icon!")
+        elif (self.selected_icon.get()):
+            self.show_message("Please select an application!")
+        else:
+            self.show_message("Please select an application and icon!")
+
+    def clear_selections(self):
+        self.selected_app.set('')
+        self.selected_icon.set('')
+        self.icon_label['image'] = None
+        self.icon_label.image = None
 
     def run(self):
         self.root.mainloop()
@@ -91,11 +110,10 @@ class ShortcutterApp:
 
 def main():
     try:
-        app = ShortcutterApp()
-        app.run()
+        ShortcutterApp().run()
     except KeyboardInterrupt:
         sys.exit(0)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
